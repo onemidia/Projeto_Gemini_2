5ba1425ccfa41e397670cfb0ece60d5f2265d585
onemidia <onemidiaindoora@gmail.com>
2025-01-30 09:51:32 -0300

    Initial commit

--- routes.py
+++ routes.py
@@ -9,7 +9,13 @@ from werkzeug.utils import secure_filename
 
 routes = Blueprint("routes", __name__)
 
-# ... (resto do seu código)
+# Configurações
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'txt'}
+
+# Função para verificar se o arquivo tem uma extensão permitida
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @routes.route('/', methods=['GET', 'POST'])
 def index():
@@ -26,7 +32,40 @@ def index():
             return redirect(request.url)
         
         if arquivo and allowed_file(arquivo.filename): # Agora arquivo está sempre definido
-            # ... (resto do seu código para processar o arquivo)
+            try:
+                os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+                arquivo.save(os.path.join(UPLOAD_FOLDER, filename))
+
+                with open(os.path.join(UPLOAD_FOLDER, filename), 'r') as file:
+                    reader = csv.reader(file, delimiter=';')
+                    produtos = []
+
+                    with next(get_db()) as db:  # Use o bloco with aqui
+                        db.query(Produto).delete()
+                        db.commit()
+                        for row in reader:
+                            try:
+                                id_produto = int(row[0])
+                                descricao = row[1]
+                                valor = float(row[2])
+                                unidade = row[3]
+                                produto = Produto(id=id_produto, codigo=id_produto, descricao=descricao, valor=valor, unidade=unidade)
+                                db.add(produto)
+                                produtos.append(produto)
+                            except ValueError:
+                                flash('Erro ao processar linha do arquivo TXT. Verifique o formato.', 'error')
+                                db.rollback()
+                                break
+                        if not produtos:
+                            return redirect(request.url)
+                        db.commit()
+
+                flash('Arquivo TXT enviado e dados atualizados com sucesso!', 'success')
+                return redirect(url_for('index'))
+
+            except Exception as e:
+                flash(f'Erro ao processar arquivo: {e}', 'error')
+                return redirect(request.url)
 
         else:
             flash('Tipo de arquivo não permitido', 'error')
