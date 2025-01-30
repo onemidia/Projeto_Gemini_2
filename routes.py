d4465e528ae5a6f7cdf60a1d46fd0315c5808596
onemidia <onemidiaindoora@gmail.com>
2025-01-30 09:44:42 -0300

    Initial commit5

--- routes.py
+++ routes.py
@@ -14,47 +14,24 @@ routes = Blueprint("routes", __name__)
 @routes.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # ... (resto do seu código)
+        arquivo = None  # Defina arquivo como None aqui
 
-        if arquivo and allowed_file(arquivo.filename):
-            filename = secure_filename(arquivo.filename)
-            try:
-                os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-                arquivo.save(os.path.join(UPLOAD_FOLDER, filename))
+        if 'file' not in request.files:
+            flash('Nenhum arquivo enviado', 'error')
+            return redirect(request.url)
 
-                with open(os.path.join(UPLOAD_FOLDER, filename), 'r') as file:
-                    reader = csv.reader(file, delimiter=';')
-                    produtos = []
+        arquivo = request.files['file'] # Move a definição para cá
+        if arquivo.filename == '':
+            flash('Nenhum arquivo selecionado', 'error')
+            return redirect(request.url)
+        
+        if arquivo and allowed_file(arquivo.filename): # Agora arquivo está sempre definido
+            # ... (resto do seu código para processar o arquivo)
 
-                    with next(get_db()) as db:  # Use o bloco with aqui
-                        db.query(Produto).delete()
-                        db.commit()
-                        for row in reader:
-                            try:
-                                id_produto = int(row[0])
-                                descricao = row[1]
-                                valor = float(row[2])
-                                unidade = row[3]
-                                produto = Produto(id=id_produto, codigo=id_produto, descricao=descricao, valor=valor, unidade=unidade)
-                                db.add(produto)
-                                produtos.append(produto)
-                            except ValueError:
-                                flash('Erro ao processar linha do arquivo TXT. Verifique o formato.', 'error')
-                                db.rollback()
-                                break
-                        if not produtos:
-                            return redirect(request.url)
-                        db.commit()
-
-                flash('Arquivo TXT enviado e dados atualizados com sucesso!', 'success')
-                return redirect(url_for('index'))
-
-            except Exception as e:
-                flash(f'Erro ao processar arquivo: {e}', 'error')
-                return redirect(request.url)
         else:
             flash('Tipo de arquivo não permitido', 'error')
             return redirect(request.url)
+
     return render_template('index.html')
 
 @routes.route('/produtos', methods=['GET'])
